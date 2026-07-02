38510e09c45780052001122cab2c158140ce7ea5
akasaki1211 <akasaki1211ab@gmail.com>
2026-07-02 11:20:32 +0900

    feat: update basic_usage.py

--- sample_scripts/basic_usage.py
+++ sample_scripts/basic_usage.py
@@ -7,5 +7,8 @@ joints = cmds.ls(sl=True)
 
 
 # Create a dynamics node for each bone section in the selected joint-chain.
+# bdn.create_dynamics_node returns the created node.
+bd_nodes = []
 for bone, end in zip(joints[:-1], joints[1:]):
-    bdn.create_dynamics_node(bone, end)
+    bd_node = bdn.create_dynamics_node(bone, end)
+    bd_nodes.append(bd_node)
